e621dcc39a760fdd7fa051425e76bed3a9ef7f39
digheyash <digheyashoff@gmail.com>
2026-05-25 14:29:08 +0530

    change

---
+++ _2_stringprb.java
@@ -0,0 +1,9 @@
+public class _2_stringprb {
+    public static void main(String[] args) {
+        String str = "Hello";
+        str = "world";
+        System.out.println(str);
+        System.out.println(str);
+
+    }
+}
