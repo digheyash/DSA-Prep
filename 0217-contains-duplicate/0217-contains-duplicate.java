5d5828610ccfb6db8ee38bf91e666c8591a74d5d
Yash Pandharinath Dighe <digheyashoff@gmail.com>
2026-07-31 00:02:42 +0530

    Time: 26 ms (8.99%), Space: 106.6 MB (42.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,14 +3,14 @@ class Solution {
         boolean flag = false ;
         HashMap< Integer , Integer > map = new HashMap<>() ;
         for ( int num : nums){
-            map.put(num,map.getOrDefault(num , 0)+1);
-        }
-        for (int no : map.keySet()){
-            if (map.get(no) > 1){
-                flag = true ;
-                break ;
+            if(map.containsKey(num)){
+                return true ;
+            }
+            else{
+                map.put(num,map.getOrDefault(num , 0)+1);
             }
         }
+       
         return flag ;
 
     }
