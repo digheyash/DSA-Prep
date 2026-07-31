b6fb07e7160c6580285198fa213ef845984e2977
digheyash <digheyashoff@gmail.com>
2026-07-31 08:40:04 +0530

    new problem

--- highestFreq.java
+++ highestFreq.java
@@ -1,17 +1,28 @@
 import java.util.* ;
 public class highestFreq {
 
-    static void highFreq (int [] arr ){
+    static String highFreq (int [] arr ){
         HashMap<Integer,Integer> freq = new HashMap<>() ;
         for (int num :arr){
             freq.put(num,freq.getOrDefault(num , 0)+1) ;
         }
-        for (int nums : freq.keySet()){
-            System.out.println( nums +" => "+freq.get(nums) ) ;
+        int maxval = -1 ;
+        int maxkey = -1 ;
+        for (int currentkey : freq.keySet()){
+           // System.out.println( nums +" => "+freq.get(nums) ) ;
+            if(freq.get(currentkey) > maxval){
+                maxval = freq.get(currentkey) ;
+                maxkey = currentkey ;
+            }
+
+
         }
+        //return maxkey ;
+        return ("Number with highest freq is : "+ maxkey + " and its frequency is : " + freq.get(maxkey)) ;
     }
     public static void main(String[] args){
         int [] arr = {1,2,3,4,2,3,4,2,4,5,6,4,4,4,6,6,7} ;
-        highFreq(arr) ;
+        System.out.println( highFreq(arr)) ;
+        //highFreq(arr) ;
     }
 }
