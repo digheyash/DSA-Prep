c93950084ce61261c991aaef76ae9d6d43147a74
Yash Pandharinath Dighe <digheyashoff@gmail.com>
2026-08-05 00:38:43 +0530

    Time: 2 ms (99.32%), Space: 47.4 MB (11.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,8 +5,9 @@ class Solution {
         for (int i = 0 ; i< nums.length ;i++){
             int compliment = target - nums[i] ;
             if(hmap.containsKey(compliment)){
-                arr[0] = i ;
-                arr[1] = hmap.get(compliment) ;
+                return new int [] {hmap.get(compliment), i } ;
+                // arr[0] = i ;
+                // arr[1] = hmap.get(compliment) ;
             }
             else{
                 hmap.put(nums[i],i) ;
