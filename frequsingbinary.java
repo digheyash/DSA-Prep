75b1a123567bdd71da0beb5602ec3f8a810eb046
digheyash <digheyashoff@gmail.com>
2026-08-31 22:11:56 +0530

    dsa prb

--- frequsingbinary.java
+++ frequsingbinary.java
@@ -32,7 +32,7 @@ public class frequsingbinary {
         return upper-low ;
     }
     public static void main(String[] args){
-        int [] arr = {1,2,3,4,4,5,6,7,8} ;
+        int [] arr = {1,2,3,4,4,5,6,6,7,8} ;
         System.out.println(nooccurence(arr ,6)) ;
 
     }
